76f2718984f200c6eda74155c57c39278f37174f
taijusanagi <taijusanagi@gmail.com>
2024-08-01 18:57:09 +0700

    test to add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+        />
         <script
           src="https://telegram.org/js/telegram-web-app.js"
           async
