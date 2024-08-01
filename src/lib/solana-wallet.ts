186e0e72e967dded0fde95001bbe7c0e502eff82
taijusanagi <taijusanagi@gmail.com>
2024-08-01 18:35:00 +0700

    use mainnet for phantom connection

--- src/lib/solana-wallet.ts
+++ src/lib/solana-wallet.ts
@@ -21,7 +21,7 @@ export const createSignInData = async () => {
     domain,
     statement: `Clicking Sign or Approve only means you have proved this wallet is owned by you. This request will not trigger any blockchain transaction or cost any gas fee.`,
     version: "1",
-    chainId: "devnet",
+    chainId: "mainnet",
   };
   return signInData;
 };
