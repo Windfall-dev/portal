d3eb348c4b733e69c940205260a480d45fe0a016
taijusanagi <taijusanagi@gmail.com>
2024-08-01 11:56:51 +0700

    remove init data for test

--- src/providers/TelegramProvider.tsx
+++ src/providers/TelegramProvider.tsx
@@ -20,9 +20,7 @@ export function TelegramProvider({ children }: { children: React.ReactNode }) {
         if (window.Telegram?.WebApp) {
           const tgWebApp = window.Telegram.WebApp;
           tgWebApp.ready();
-          // const initData = tgWebApp.initData;
-          const initData =
-            "user=%7B%22id%22%3A596533929%2C%22first_name%22%3A%22Taiju%22%2C%22last_name%22%3A%22Sanagi%22%2C%22username%22%3A%22taijusanagi%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=-9202674396613559208&chat_type=private&auth_date=1722478612&hash=480349057fedbce68bf3f4771d92ec9ebe217d61983a2c9551565e4e4ae3dba9";
+          const initData = tgWebApp.initData;
           if (initData) {
             const accessToken =
               await actions.getAccessTokenByTelegramInitData(initData);
