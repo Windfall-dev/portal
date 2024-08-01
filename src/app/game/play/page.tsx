679949574b341afab0cf475005c6e6095425a4b1
taijusanagi <taijusanagi@gmail.com>
2024-08-01 18:24:32 +0700

    fix build

--- src/app/game/play/page.tsx
+++ src/app/game/play/page.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import { WebGLGameWithAuth } from "@/components/WebGLGameWithAuth";
 
-export function GamePlayPage() {
+export default async function GamePlayPage() {
   return <WebGLGameWithAuth url="/webgl/index.html" />;
 }
-
-export default GamePlayPage;
