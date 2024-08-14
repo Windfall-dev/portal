416d271535cd8ebee0a919f3c51507ac43aa6b56
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-08-14 17:54:57 +0900

    change game/play to fullscreen WebGLGameWithAuth

--- src/app/game/play/page.tsx
+++ src/app/game/play/page.tsx
@@ -1,34 +1,7 @@
-"use client";
-
-import Image from "next/image";
-import { useRouter } from "next/navigation";
 import React from "react";
 
-// import { WebGLGameWithAuth } from "@/components/WebGLGameWithAuth";
+import { WebGLGameWithAuth } from "@/components/WebGLGameWithAuth";
 
-const CloseButton = () => {
-  const pathname = useRouter();
-
-  return (
-    <div
-      onClick={() => pathname.push("/game")}
-      className="absolute top-16 right-4 z-10 rounded-full p-2 w-10 h-10"
-      aria-label="Close"
-    ></div>
-  );
-};
 export default async function GamePlayPage() {
-  // return <WebGLGameWithAuth url="/webgl/index.html" />;
-  return (
-    <div className="relative w-full h-screen overflow-hidden">
-      <Image
-        src="/_Game_play.png"
-        fill={true}
-        alt="Game play"
-        objectFit="cover"
-        className="object-top"
-      />
-      <CloseButton />
-    </div>
-  );
+  return <WebGLGameWithAuth url="/webgl/index.html" />;
 }
