c29853a1c976243a1babd8f66c6c6806421ebd35
funkyenough <sys.junichi.koyama@gmail.com>
2024-12-05 01:18:25 +0900

    To check for errors 3

--- services/auth/src/lib/auth.ts
+++ services/auth/src/lib/auth.ts
@@ -8,8 +8,9 @@ export interface AuthTokenPayload {
 const secret = process.env.AUTH_SECRET!;
 
 export function createAccessToken(payload: AuthTokenPayload): string {
-  console.log("AUTH_SECRET:", secret);
-  return jwt.sign(payload, secret);
+  // console.log("AUTH_SECRET:", secret);
+  // TODO:キーを環境変数で取得する AWSだと読み込めない
+  return jwt.sign(payload, "36rruVrNSN7ybXKLprhtGbXK6j8HSfGvzE0bRMaNx0Q=");
 }
 
 export function verifyAccessToken(token: string): AuthTokenPayload {
