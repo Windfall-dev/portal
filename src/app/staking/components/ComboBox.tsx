95c52c9efcdde52c98182af6c8c14d574df6c5ae
funkyenough <14842981+funkyenough@users.noreply.github.com>
2024-12-05 13:18:55 +0900

    make vaultType UI more palatable and make confirm dialog text dynamic

--- src/app/staking/components/ComboBox.tsx
+++ src/app/staking/components/ComboBox.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { Check, ChevronsUpDown } from "lucide-react";
-import * as React from "react";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -18,26 +19,39 @@ import {
 import { cn } from "@/lib/utils";
 
 import { useVaultContext } from "../context/VaultContext";
-import { VAULT_CONFIGS, VaultConfig } from "../utils/vaultUtils";
+import { VAULT_CONFIGS, VaultConfig, getVaultType } from "../utils/vaultUtils";
 
 export function ComboBox() {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const { selectedVault, setSelectedVault } = useVaultContext();
+
+  // テスト用
+  useEffect(() => {
+    getVaultType(selectedVault);
+  }, [selectedVault]);
 
   return (
     <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
+      <PopoverTrigger asChild className="w-16">
         <Button
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-32 justify-between px-3"
         >
-          {selectedVault}
+          <div className="flex items-center space-x-2">
+            <Image
+              src={selectedVault.logo}
+              alt={selectedVault.vaultName}
+              width={24}
+              height={24}
+            />
+            <p>{selectedVault.vaultName}</p>
+          </div>
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-32 p-0">
         <Command>
           <CommandList>
             <CommandGroup>
@@ -47,20 +61,28 @@ export function ComboBox() {
                   value={vault.vaultName}
                   onSelect={(currentValue) => {
                     setSelectedVault(
-                      currentValue === selectedVault ? "" : currentValue,
+                      VAULT_CONFIGS[currentValue] === selectedVault
+                        ? VAULT_CONFIGS["SOL"]
+                        : VAULT_CONFIGS[currentValue],
                     );
                     setOpen(false);
                   }}
                 >
+                  <div className="flex items-center space-x-2">
+                    <Image
+                      src={vault.logo}
+                      alt={vault.vaultName}
+                      width={24}
+                      height={24}
+                    />
+                    <p>{vault.vaultName}</p>
+                  </div>
                   <Check
                     className={cn(
                       "mr-2 h-4 w-4",
-                      selectedVault === vault.vaultName
-                        ? "opacity-100"
-                        : "opacity-0",
+                      selectedVault === vault ? "opacity-100" : "opacity-0",
                     )}
                   />
-                  {vault.vaultName}
                 </CommandItem>
               ))}
             </CommandGroup>
--- src/app/staking/components/StakingTabCards.tsx
+++ src/app/staking/components/StakingTabCards.tsx
@@ -16,16 +16,16 @@ export function StakingTabCard({ actionType }: TabCardProps) {
       <div className="flex flex-col rounded-md border border-border">
         <h3 className="px-5 py-4 capitalize">{actionType}</h3>
         <div className="px-5">
-          <div className="flex items-center rounded-lg border-2 px-3 focus-within:border-[#FF9100] focus-within:ring-[#FF9100]">
+          <div className="flex items-center rounded-lg border-2 focus-within:border-[#FF9100] focus-within:ring-[#FF9100]">
             <input
               id="amount-input"
-              className={`body h-9 w-full border-0 px-0 py-0 ${
+              className={`body h-9 w-full border-0 px-3 py-0 ${
                 amount ? "text-black" : "text-gray"
               } focus:text-black focus:outline-none`}
               placeholder={`Enter amount to ${actionType}`}
               onChange={(e) => setAmount(e.target.value)}
             />
-            <div className="flex flex-shrink-0 justify-center rounded bg-gray text-center">
+            <div className="flex min-w-[8rem] flex-shrink-0 justify-end rounded bg-gray text-center">
               <ComboBox />
             </div>
           </div>
